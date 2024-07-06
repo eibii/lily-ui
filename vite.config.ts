ea08b889b853c135a0a1d7d4edd2c3e340c83ff9
Edu <dev@eibii.com>
2024-07-06 14:37:55 -0300

    feat: Add lodash-es as a dependency

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: ['vue', 'lodash-es', '@vueuse/core', 'bootstrap-icons', 'tailwindcss-animated'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
